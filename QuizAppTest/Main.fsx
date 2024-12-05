7033e5c63c673c0ba08225a0f08e37712011262e
MoaazMahmoud22 <mohazmahmod@gmail.com>
2024-12-05 13:48:08 +0200

    Add project files.

---
+++ QuizAppTest/Main.fsx
@@ -0,0 +1,10 @@
+﻿module Main
+
+open System
+open System.Windows.Forms
+open QuizForm
+
+[<EntryPoint>]
+let main _ = 
+    Application.Run(new QuizForm())
+    0
---
+++ QuizAppTest/Models.fsx
@@ -0,0 +1,6 @@
+﻿module Models
+
+type Question = 
+    { QuestionText: string
+      Choices: string list option // Some for MCQs, None for written answers
+      CorrectAnswer: string }
