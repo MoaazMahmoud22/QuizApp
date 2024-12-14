27840792090a46770d382f8d98428acc8dbb203f
mostafa8899 <120140167+mostafa8899@users.noreply.github.com>
2024-12-14 19:44:13 +0200

    Add files via upload

---
+++ Questions.fsx
@@ -0,0 +1,17 @@
+﻿module Questions
+
+open Models
+
+let questions = 
+    [ { QuestionText = "What is the capital of Egypt?"
+        Choices = Some ["Cairo"; "Alexandria"; "Luxor"; "Aswan"]
+        CorrectAnswer = "Cairo" }
+      { QuestionText = "What is the chemical formula for water?"
+        Choices = None
+        CorrectAnswer = "H2O" }
+      { QuestionText = "Name a programming language that starts with 'P'."
+        Choices = None
+        CorrectAnswer = "Python" }
+      { QuestionText = "What is an operating system?"
+        Choices = None
+        CorrectAnswer = "interface between the hardware and application programs" }]
